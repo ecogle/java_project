bd886b93b1691731560d1918db6f386b4c689730
Chad Ogle <ecogle@c3devloping.com>
2017-02-03 14:10:57 -0600

    LogFile class created to write the login entries to the file.

---
+++ Java Final/src/pkgfinal2/LogFile.java
@@ -0,0 +1,28 @@
+package pkgfinal2;
+
+import java.io.*;
+import java.time.*;
+import java.nio.file.*;
+import java.nio.*;
+import java.time.format.DateTimeFormatter;
+
+
+public class LogFile {
+    
+    public static void write(String username,LogEvents events){
+        StringBuilder sb = new StringBuilder();
+            sb.append(events + "\t");
+            sb.append(username + "\t");
+            sb.append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("MMM dd yyy\th:mma")));
+            sb.append("\n");
+        
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(System.getProperty("user.dir")+"/src/pkgfinal2/log.txt",true))){
+            bw.write(sb.toString());
+            bw.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
