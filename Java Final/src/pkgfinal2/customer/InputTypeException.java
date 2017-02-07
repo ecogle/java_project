5912bf001e34dd090e75da40c8c8b5f615cea3a8
ecogle <ecogle@c3developing.com>
2017-02-07 13:11:38 -0600

    Custom exception built for the error in input

--- Java Final/src/pkgfinal2/customer/InputTypeException.java
+++ Java Final/src/pkgfinal2/customer/InputTypeException.java
@@ -2,4 +2,11 @@ package pkgfinal2.customer;
 
 public class InputTypeException extends Exception{
 
+    public InputTypeException(){
+        super("Input invalid");
+    }
+
+    public InputTypeException(String str){
+        super(str);
+    }
 }
