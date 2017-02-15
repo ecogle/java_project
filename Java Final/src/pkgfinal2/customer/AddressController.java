c9f612b0eaeb172798d7ad002c42869ae0a31679
Chad Ogle <ecogle@c3devloping.com>
2017-02-15 14:02:55 -0600

    added error checking ability

--- Java Final/src/pkgfinal2/customer/AddressController.java
+++ Java Final/src/pkgfinal2/customer/AddressController.java
@@ -168,5 +168,19 @@ public class AddressController {
     }
 
 
+    public void updateCustomerAddress(){
+        String sql = "update address set address = ?,address2 = ?,phone=?,postalCode = ? where addressId = ?";
 
+        try(PreparedStatement ps = MySQLDatabase.getMySQLConnection().prepareStatement(sql)){
+            ps.setString(1,this.completeCustomer.getAddress());
+            ps.setString(2,this.completeCustomer.getAddress2());
+            ps.setString(3,this.completeCustomer.getPhone());
+            ps.setString(4,this.completeCustomer.getPostalCode());
+            ps.setInt(5,this.completeCustomer.getFkAddressId());
+            ps.execute();
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
